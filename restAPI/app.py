5d1c15453b4e4c3d31581d71a480496e73c343e9
Sebastian Yepes Sanchez <syepes@MacBook-Pro-de-Sebastian.local>
2022-10-17 13:35:38 -0500

    feat: added basic flask rest api boiler plate and some get and post methods

---
+++ restAPI/app.py
@@ -0,0 +1,67 @@
+from http.client import REQUEST_HEADER_FIELDS_TOO_LARGE
+from flask import Flask, request
+#file name and variable name should always be app
+app = Flask(__name__)
+
+stores = [
+    {
+        "name":"My store",
+        "items": [
+            {
+            "name" : "Chair",
+            "price": 15.99
+        }
+        ]
+    }
+]
+
+##this is a endpoint definition
+#defining get request for server/store location
+@app.get("/store")
+def get_stores():
+    return {"Stores": stores}
+
+##JSON IS ALWAYS A STRING!!
+
+#defining post request for server/store location
+@app.post("/store")
+def create_store():
+    ##this convert json string to a python directionary and also request data passed
+    request_data = request.get_json()
+    new_store = {
+        "name": request_data["name"],
+         "items":[],
+    }
+    ##On response codes 200 represents everything ok
+    ##and with 201 code it means evetything ok, i accept your data
+    return new_store, 201
+
+## the <string:name> allowus to pass arguments via url and 
+##then use it on function
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item= {
+                "name": request_data["name"], 
+                "price": request_data["price"],
+                }
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
+
+@app.get("/store/<string:name>/item")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return{store}
+    return {"message": "Store not found"}, 404
+
+@app.get("/store/<string:name>/item")
+def get_store_items(name):
+    for store in stores:
+        if store["name"] == name:
+            return{"items": store["items"]}
+    return {"message": "Store not found"}, 404
+    
---
+++ restAPI/random.py
@@ -0,0 +1,4 @@
+#python3 -m venv .venv to create a virtual enviroment with .venv name 
+#then pip install flask
+#flask run to run the app after the boiler plate
+##For moaul exploratori testing INsombia is an amazing client as well as postman
