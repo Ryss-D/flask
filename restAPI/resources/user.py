758acd688527ab622aa42ed502e3d67beb71ac6c
Ryss-D <70545185+Ryss-D@users.noreply.github.com>
2023-01-21 23:24:34 -0500

    feat: created login endpoint who also creates and return jwts

--- restAPI/resources/user.py
+++ restAPI/resources/user.py
@@ -2,6 +2,7 @@ from flask_views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib_hash import pbkdf2_sha256
 ## pass lib will allow us to hash passwords
+from flask_jwt_extended import create_access_token
 
 from db import db
 from models import UserModel
@@ -26,6 +27,29 @@ class UserRegister(MethodView):
         db.session.commit()
 
         return {"message": "User created succesfully"}, 201
+
+
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data);
+        user = UserModel.query.filter(
+            UserModel.username == user_data["username"]
+        ).first()
+
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            ##with verify we are not unhashing the passwrod we are
+            ##just verifying that the passed password hashed is equal to the 
+            ##hash stored
+            access_token = create_access_token(idendity=user.id)
+            ## we are storing user id in the jwt
+            ##ensuring that this token in only valid for the user who creates it 
+            return {"access_token": access_token}
+        abort(401, message="Invaid credentials.")
+
+
+
+
 
 @blp.route("/user/<int:user_id>")
 class User(MethodView):
